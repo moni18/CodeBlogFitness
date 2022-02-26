6622b658a731914bfc03f2efc48d54c417e0297a
moni18 <78638274+moni18@users.noreply.github.com>
2022-02-26 20:24:31 +0600

    Добавьте файлы проекта.

---
+++ CodeBlogFitness.CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeBlogFitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
