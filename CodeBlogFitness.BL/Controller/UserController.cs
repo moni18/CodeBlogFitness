15ab02fd5a08b71f1bc80b469d79103a3711a270
moni18 <78638274+moni18@users.noreply.github.com>
2022-02-27 23:22:42 +0600

    Добавлено модульное тестирование для контроллера пользователя

--- CodeBlogFitness.BL/Controller/UserController.cs
+++ CodeBlogFitness.BL/Controller/UserController.cs
@@ -53,8 +53,8 @@ namespace CodeBlogFitness.BL.Controller
             var formatter = new BinaryFormatter();
 
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
-            {
-                if (formatter.Deserialize(fs) is List<User> users)
+            {                
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<User> users)
                 {
                     return users;
                 }
